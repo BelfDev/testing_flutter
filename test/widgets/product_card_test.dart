c08ac33e17adc71d86d57aff98a2b563e70a77ce
Pedro Belfort <pedro.belfort@aboutyou.com>
2021-07-11 20:12:13 +0200

    Introduce widget test setup

---
+++ test/widgets/product_card_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('ProductCard:', () {
+
+    testWidgets('description', (WidgetTester tester) async {
+
+    });
+
+  });
+}
